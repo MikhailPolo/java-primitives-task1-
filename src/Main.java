2c9066ada6bd8590eae9b552fca194dcb2203a17
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2022-10-09 14:40:04 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    int price = 900; //стоимость билета
+
+    int bonuse = price / 20; //1 миля за каждые 20 рублей стоимости бмлета
+
+    System.out.println("Итого бонусов:" + bonuse);
+    }
+
+}
